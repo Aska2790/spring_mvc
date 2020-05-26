0c28cbcbcf514d7787aa3da4c682aafc769fb7e7
Aska2790 <arslanannaev7@gmail.com>
2020-05-26 22:44:17 +0300

    empty project

---
+++ src/main/java/com/aska/development/App.java
@@ -0,0 +1,18 @@
+package com.aska.development;
+
+public class App {
+    //region Поля
+    //endregion
+
+    //region Свойства
+    //endregion
+
+    //region Конструктор
+    //endregion
+
+    //region Методы
+    //endregion
+
+    //region Внутренние классы
+    //endregion
+}
